abf196246287ee57f114d7e5ee0275fc826caf5f
Thoufeeque <thoufeeque.rafique@gmail.com>
2025-05-30 15:10:11 +0200

    moved steps to individual methods

--- src/test/java/StandAloneTest.java
+++ src/test/java/StandAloneTest.java
@@ -13,12 +13,12 @@ import java.nio.file.StandardCopyOption;
 import java.time.Duration;
 import java.util.List;
 
-import static org.openqa.selenium.support.locators.RelativeLocator.with;
 
 public class StandAloneTest {
-    public static void main(String[] args) throws IOException {
-        String orderItem = "ZARA COAT 3";
-        String country = "India";
+    static String orderItem = "ZARA COAT 3";
+    static String country = "India";
+
+    static WebDriver driver_init() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("user-data-dir=C:/Users/trafique/selenium-profile");
@@ -26,54 +26,87 @@ public class StandAloneTest {
         options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"}); // Makes it look more human
         options.addArguments("--incognito");
         WebDriver driver = new ChromeDriver(options);
+        return driver;
+    }
+
+    static void login(WebDriver driver) {
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://rahulshettyacademy.com/client");
+        driver.findElement(By.id("userEmail")).sendKeys("a@gmail.com");
+        driver.findElement(By.id("userPassword")).sendKeys("easypw");
+        driver.findElement(By.id("login")).click();
+    }
+
+    static void addToCart(WebDriver driver, WebDriverWait wait) {
+        WebElement itemCard = driver.findElement(By.className("card-body"));
+        System.out.println("itemCard.getText() = " + itemCard.getText());
+        if (itemCard.getText().contains(orderItem)) {
+            itemCard.findElement(By.xpath(".//button[contains(text(),'Add To Cart')]")).click();
+            System.out.println(wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[aria-label='Product Added To Cart']"))).getText());
+        }
+    }
+
+    static void gotoCart(WebDriverWait wait) {
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".fa-shopping-cart"))).click();
+    }
+
+    static void verifyCartItems(WebDriver driver) {
+        String cartItemText = driver.findElement(By.cssSelector(".infoWrap")).getText();
+        Assert.assertTrue(cartItemText.contains(orderItem));
+    }
+
+    static void checkout(WebDriver driver) {
+        driver.findElement(By.cssSelector(".subtotal button")).click();
+
+        String placeOrderText = driver.findElement(By.cssSelector(".item__title")).getText();
+        Assert.assertTrue(placeOrderText.contains(orderItem));
+
+        driver.findElement(By.cssSelector("[placeholder='Select Country']")).sendKeys(country);
+
+        List<WebElement> countryList = driver.findElements(By.cssSelector(".list-group-item"));
+        for (WebElement listCountry : countryList) {
+            if (listCountry.getText().equals(country)) {
+                listCountry.click();
+                break;
+            }
+        }
+        driver.findElement(By.cssSelector(".action__submit")).click();
+    }
+
+    static void verifyOrder(WebDriver driver) {
+        String orderId = driver.findElement(By.cssSelector("label[class='ng-star-inserted']")).getText().split(" ")[1];
+
+        driver.findElement(By.xpath("//button[contains(text(),'ORDERS')]")).click();
+
+        List<WebElement> finalOrderIds = driver.findElements(By.cssSelector("tbody th"));
+        for (WebElement finalId: finalOrderIds) {
+            if (finalId.getText().contains(orderId)) {
+                System.out.println("Verified ordered order id");
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        WebDriver driver = driver_init();
+
         TakesScreenshot ss = (TakesScreenshot) driver;
+
         try {
-            driver.manage().window().maximize();
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.get("https://rahulshettyacademy.com/client");
-            driver.findElement(By.id("userEmail")).sendKeys("a@gmail.com");
-            driver.findElement(By.id("userPassword")).sendKeys("easypw");
-            driver.findElement(By.id("login")).click();
+            login(driver);
 
-            WebElement itemCard = driver.findElement(By.className("card-body"));
-            System.out.println("itemCard.getText() = " + itemCard.getText());
-            if (itemCard.getText().contains(orderItem)) {
-                itemCard.findElement(By.xpath(".//button[contains(text(),'Add To Cart')]")).click();
-                System.out.println(wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[aria-label='Product Added To Cart']"))).getText());
-            }
+            addToCart(driver, wait);
 
-            wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".fa-shopping-cart"))).click();
+            gotoCart(wait);
 
-            String cartItemText = driver.findElement(By.cssSelector(".infoWrap")).getText();
-            Assert.assertTrue(cartItemText.contains(orderItem));
+            verifyCartItems(driver);
 
-            driver.findElement(By.cssSelector(".subtotal button")).click();
+            checkout(driver);
 
-            String placeOrderText = driver.findElement(By.cssSelector(".item__title")).getText();
-            Assert.assertTrue(placeOrderText.contains(orderItem));
+            verifyOrder(driver);
 
-            driver.findElement(By.cssSelector("[placeholder='Select Country']")).sendKeys(country);
-
-            List<WebElement> countryList = driver.findElements(By.cssSelector(".list-group-item"));
-            for (WebElement listCountry : countryList) {
-                if (listCountry.getText().equals(country)) {
-                    listCountry.click();
-                    break;
-                }
-            }
-
-            driver.findElement(By.cssSelector(".action__submit")).click();
-            String orderId = driver.findElement(By.cssSelector("label[class='ng-star-inserted']")).getText().split(" ")[1];
-
-            driver.findElement(By.xpath("//button[contains(text(),'ORDERS')]")).click();
-
-            List<WebElement> finalOrderIds = driver.findElements(By.cssSelector("tbody th"));
-            for (WebElement finalId: finalOrderIds) {
-                if (finalId.getText().contains(orderId)) {
-                    System.out.println("Verified order id ordered");
-                }
-            }
         } catch (Exception e) {
             e.printStackTrace();
             File src = ss.getScreenshotAs(OutputType.FILE);
